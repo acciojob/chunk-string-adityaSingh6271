314095ca61a0439a3f941026bf4b694b0e0b988d
Aditya Singh <131813027+adityaSingh6271@users.noreply.github.com>
2024-01-13 11:33:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function stringChop(str, size) {
   // your code here
+	if (str === null) {
+    return [];
+  }
+ const result = [];
+  for (let i = 0; i < str.length; i += size) {
+    const chunk = str.slice(i, i + size);
+    result.push(chunk);
+  }
+
+  return result;
 }
 
 // Do not change the code below
